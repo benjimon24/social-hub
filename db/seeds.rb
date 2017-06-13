c729d49902794bea053c5a81fb4ab29ed49bf10e
benjimon24 <benji102491@gmail.com>
2017-06-13 19:11:47 -0400

    Create migration and models for Users and groups

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(username: "kane", email: "kane@gmail.com", password: "password")
+User.last.groups.create(name: "yay")
